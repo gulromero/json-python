2088cdb6a38bd9708a365e397f3fe4dc736af970
Gülsüm <gulsumerd38@gmail.com>
2024-02-09 17:31:47 +0100

    opg 4 og 5

--- jsonpython.py
+++ jsonpython.py
@@ -4,7 +4,7 @@ import json
 #PYTHON JSON EXERCISES - PART 1:
 
 #Creating a dictionary
-myobject = {"ID": 1, "Name": "Gülsüm", "Age": 25, "Description": "My fav color is pink"}
+myobject = {"ID": 1, "Name": "Gulsum", "Age": 25, "Description": "My fav color is pink"}
 
 #Printing the dictionary
 print(myobject["ID"], myobject["Name"], myobject["Age"], myobject["Description"])
@@ -23,3 +23,23 @@ print(manualSerialized)
 
 
 #PART 3: DESERIALIZATION
+
+#Deserializing and storing the result into anoter variable. 
+manualDeserialized = json.loads(manualSerialized)
+
+#printing the deserilized object
+print(manualDeserialized)
+
+#PART 4: SPECIAL CHARACTERS
+
+manualSerialized = {"ID": 1, "Name": "Gulsu\"m", "Age": 25, "Description": "My fav color is pink"}
+print(manualSerialized)
+
+#PART 5: Working with others Json
+
+#We're provided a JSON string that we want to deserialize into a dictionary.
+
+json_string = {"Name":"Move Cars","Address":"MagleGaardsvej 2","Cars":[{"Brand":"BMW","Model":"330e","Color":"Green","Mileage":45721},{"Brand":"VW","Model":"Golf","Color":"Red","Mileage":20},{"Brand":"Ford","Model":"Galaxy","Color":"Black","Mileage":124326}],"Employees":[{"Name":"Move","Salary":1000000,"MonthsEmployed":28,"JobAreas":["President","Mechanic"]},{"Name":"Not Move","Salary":100,"MonthsEmployed":13,"JobAreas":["Vice-President","Mechanic"]}]}
+
+data = json.loads(json_string)
+
